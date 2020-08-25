0b0ba85ddc739b2b69cb5bd8dd98b6f7cd1b0c6a
rigaytan <rigaytan@gmail.com>
2020-08-25 01:28:33 -0500

    first commit

---
+++ Clase_2/src/clase_2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+    //inicia declaracion de variables
+    float edad = 0;
+    float dia = 24;
+    float suma = 0;
+    //inicia el codigo del algoritmo
+    printf("Hola bienvenido al sistema\n");
+    printf("Para comenzar escribe tu edad: ");
+    scanf("%f",&edad);
+    suma = edad + 10;
+    printf("escribe la fecha del dia de hoy: ");
+    scanf("%f",&dia);
+    printf("La edad que %f gracias, hoy es %f\n",edad,dia);
+    printf("Dentro de diez anios tendras %f\n",suma);
+    printf("programa finalizado");
+    return 0;
+}
---
+++ ejemplo_1/src/ejemplo.c
@@ -0,0 +1,7 @@
+#include <stdio.h> //declaracion de librerias
+int main() //Funcion principal
+{ //se abre llave para la funcion principal
+printf("pais\tnombre\tanimal\tcolor\n"); //imprimir mensaje en pantalla
+printf("Mexico\tKike\tLeon\tamarillo");
+return 0; //Retorna un valor, cero para este caso
+} //se cierra llave para la funcion principal
